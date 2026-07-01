8f8a351c2285860a55a1b63388d95ede312829c7
Abhishek <iabhishek023@gmail.com>
2026-07-01 12:33:39 +0530

    Customized DTO

--- src/main/java/com/streamwise/streamwise/dto/AuthResponse.java
+++ src/main/java/com/streamwise/streamwise/dto/AuthResponse.java
@@ -1,4 +1,15 @@
 package com.streamwise.streamwise.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
+    private String token;
+    private String email;
+    private String name;
+    private String message;
 }
--- src/main/java/com/streamwise/streamwise/dto/MovieSearchRequest.java
+++ src/main/java/com/streamwise/streamwise/dto/MovieSearchRequest.java
@@ -1,4 +1,15 @@
 package com.streamwise.streamwise.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class MovieSearchRequest {
+
+    @NotBlank(message = "Search query is required")
+    @Size(min = 3, max = 500, message = "Query must be between 3 and 500 characters")
+    private String query;
+
+    private String userEmail;
 }
